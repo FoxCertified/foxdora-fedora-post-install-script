c7630ca3cdf5e05e4e4034a3135fe26c40c10e17
Fox! <me@foxcertified.gay>
2026-06-20 21:16:11 -0500

    Delete scripts directory

--- scripts/subscripts/extra-utility-meta.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Fedora
-sudo dnf -y install video-downloader
-##Flatpak
-flatpak -y install com.obsproject.Studio com.github.wwmm.easyeffects
-  echo "Installed extra utilities"
-  sleep 3
