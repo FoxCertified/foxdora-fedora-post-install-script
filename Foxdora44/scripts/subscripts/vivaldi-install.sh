949c25633174234e5be863fe1eac61c6a8663e5e
Fox! <me@foxcertified.gay>
2026-07-07 22:35:11 -0500

    Delete Foxdora44 directory

--- Foxdora44/scripts/subscripts/vivaldi-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://downloads.vivaldi.com/stable/vivaldi-stable-8.0.4033.54-1.x86_64.rpm
-sudo dnf -y in ./vivaldi-stable-8.0.4033.54-1.x86_64.rpm
-rm ./vivaldi-stable-8.0.4033.54-1.x86_64.rpm
-sudo dnf -y update --refresh
-  echo "Installed Vivaldi Web Browser"
-  sleep 3
